3d8090e3920828803bf2a7ea29553dba8dab7f56
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2025-10-10 14:20:30 +0530

    Time: 1 ms (92.18%) | Memory: 60.8 MB (23.23%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -7,13 +7,13 @@ var findMaxConsecutiveOnes = function(nums) {
     for(let i=0;i<nums.length;i++){
         if(nums[i]===1){
             current++;
-            if(current>result){
-              result=  current;
-            }
         }
         else{
+            if(current>result)
+              result=  current;
+            
             current=0;
         }
     }
-    return result;
+    return result > current ? result : current;
 };
