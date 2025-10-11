6d66097ffd3ed8934dad0d6ea7292f223019e7f9
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2025-10-11 10:36:44 +0530

    Time: 1 ms (28.36%) | Memory: 56 MB (61.00%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -5,3 +5,7 @@
 var isPowerOfTwo = function(n) {
     return n > 0 && (n & (n - 1)) === 0;
 };
+console.log(isPowerOfTwo(1));  
+console.log(isPowerOfTwo(16)); 
+console.log(isPowerOfTwo(3));  
+console.log(isPowerOfTwo(0));
