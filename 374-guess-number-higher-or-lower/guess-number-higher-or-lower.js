e44c1c568644769ca909c8da867a6370ace3ae62
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2025-10-16 18:02:25 +0530

    Time: 48 ms (28.47%) | Memory: 55 MB (7.23%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.js
@@ -17,9 +17,9 @@ var guessNumber = function(n) {
         let mid=Math.floor((start+end)/2)
         let res=guess(mid)
         if(res===0)return mid;
-        else if(res==-1)
+        else if(res===-1)
             end=mid-1
-        else
+        else if(res===1)
             start=mid+1
     }
 };
