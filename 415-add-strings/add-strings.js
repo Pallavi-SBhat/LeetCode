db18ceee6caab6107bb5bd5bf129103b65b859ab
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2025-10-18 09:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (84.97%) - LeetSync

--- 415-add-strings/add-strings.js
+++ 415-add-strings/add-strings.js
@@ -4,5 +4,6 @@
  * @return {string}
  */
 var addStrings = function(num1, num2) {
-    return (BigInt(num1) + BigInt(num2)).toString()
+    let sum_ofTwoString=(BigInt(num1) + BigInt(num2))
+    return sum_ofTwoString.toString()
 };
