5dc3823534147a40d87b464f82534f8afd5578e6
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-02-11 21:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 58.6 MB (99.63%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,10 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let left=0,right=s.length-1;
-    while(left<right){
-        [s[left],s[right]]=[s[right],s[left]];
-        left++;
-        right--;
-    }
+    // let left=0,right=s.length-1;
+    // while(left<right){
+    //     [s[left],s[right]]=[s[right],s[left]];
+    //     left++;
+    //     right--;
+    // }
+    return s.reverse();
 };
