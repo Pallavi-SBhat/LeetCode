dd8260be27e1b127e66b115594303a59c49a09b2
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-02-15 19:13:46 +0530

    Time: 1 ms (81.44%) | Memory: 58.5 MB (63.63%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -4,8 +4,7 @@
  */
 var largestOddNumber = function(num) {
     for(let i=num.length-1;i>=0;i--){
-        let digit=num[i];
-        if(digit%2==1){
+        if(num[i]%2==1){
             return num.slice(0,i+1);
         }
     }
