485fa7cf465aeb03bf06b5f510df8778d2de0cd8
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-02-26 19:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (100.00%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -3,27 +3,17 @@
  * @return {number}
  */
 var numSteps = function(s) {
-     let steps = 0;
-    s = s.split("");
-
-    while (s.length > 1) {
-        if (s[s.length - 1] === "0") {
-            s.pop(); // divide by 2
-        } else {
-            // add 1
-            let i = s.length - 1;
-            while (i >= 0 && s[i] === "1") {
-                s[i] = "0";
-                i--;
-            }
-            if (i >= 0) {
-                s[i] = "1";
-            } else {
-                s.unshift("1");
-            }
+   let steps = 0;
+    let carry = 0;
+    for (let i = s.length -1; i>0;i--){
+        const bit = s[i] - '0';
+        if(bit + carry === 1){
+            steps += 2;
+            carry = 1;
+        }else{
+            steps += 1;
         }
-        steps++;
     }
 
-    return steps;
+    return steps + carry;
 };
