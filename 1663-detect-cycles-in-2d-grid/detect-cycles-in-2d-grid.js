76ea9b750f3d905d1263dcf319f36627fa88af86
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-04-26 09:09:40 +0530

    Time: 160 ms (55.00%) | Memory: 94.6 MB (72.50%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[][]} grid
+ * @return {boolean}
+ */
+const dirs = [[-1,0],[1,0],[0,-1],[0,1]];
+
+var containsCycle = function(grid) {
+    const n=grid.length, m=grid[0].length;
+    const dfs = function(type, i, j, previ, prevj){
+        if(i<0 || j<0 || i==n || j==m) return false;
+        if(grid[i][j]!=type) return grid[i][j]==type.toUpperCase();
+        grid[i][j]=grid[i][j].toUpperCase();
+        for(let dir of dirs){
+            if(i+dir[0]==previ && j+dir[1]==prevj) continue;
+            if(dfs(type,i+dir[0],j+dir[1],i,j)) return true;
+        }
+        return false;
+    }
+    
+    for(let i=0; i<n; i++)
+        for(let j=0; j<m; j++){
+            if(grid[i][j]==grid[i][j].toUpperCase()) continue;
+            if(dfs(grid[i][j], i, j, -1, -1)) return true;
+        }
+    return false;
+};
