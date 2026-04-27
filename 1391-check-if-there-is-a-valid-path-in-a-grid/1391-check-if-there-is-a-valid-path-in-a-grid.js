9a32715ff7d8a5b27cf198cda132273c44bed9f1
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-04-27 17:31:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.js
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.js
@@ -2,35 +2,43 @@
  * @param {number[][]} grid
  * @return {boolean}
  */
-var hasValidPath = function(grid) {
-    const m = grid.length, n = grid[0].length;
-    const parent = Array.from({ length: m * n }, (_, i) => i);
-    const rank = new Array(m * n).fill(0);
+const hasValidPath = grid => {
+    const m = grid.length;
+    const n = grid[0].length;
+    if (m === 1 && n === 1) return true;
 
-    const find = (i) => {
-        if (parent[i] === i) return i;
-        return parent[i] = find(parent[i]);
+    const trans = [
+        [],
+        [-1, 1, -1, 3],
+        [0, -1, 2, -1],
+        [3, 2, -1, -1],
+        [1, -1, -1, 2],
+        [-1, 0, 3, -1],
+        [-1, -1, 1, 0]
+    ];
+
+    const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    const inits = [[], [1, 3], [0, 2], [2, 3], [1, 2], [0, 3], [0, 1]];
+
+    const check = state => {
+        let [r, c] = dirs[state];
+        const B = new Uint8Array(m * n);
+        B[0] = 1;
+
+        while (grid[r]?.[c]) {
+            if (B[r * n + c]) return false;
+            B[r * n + c] = 1;
+
+            state = trans[grid[r][c]][state];
+            if (state === -1) return false;
+            if (r === m - 1 && c === n - 1) return true;
+
+            const [dr, dc] = dirs[state];
+            r += dr;
+            c += dc;
+        }
+        return false;
     };
 
-    const union = (i, j) => {
-        let rootI = find(i), rootJ = find(j);
-        if (rootI !== rootJ) {
-            if (rank[rootI] < rank[rootJ]) parent[rootI] = rootJ;
-            else if (rank[rootI] > rank[rootJ]) parent[rootJ] = rootI;
-            else { parent[rootI] = rootJ; rank[rootJ]++; }
-        }
-    };
-
-    for (let r = 0; r < m; r++) {
-        for (let c = 0; c < n; c++) {
-            let idx = r * n + c;
-            // check right
-            if (c + 1 < n && [1, 4, 6].includes(grid[r][c]) && [1, 3, 5].includes(grid[r][c+1]))
-                union(idx, idx + 1);
-            // check down
-            if (r + 1 < m && [2, 3, 4].includes(grid[r][c]) && [2, 5, 6].includes(grid[r+1][c]))
-                union(idx, idx + n);
-        }
-    }
-    return find(0) === find(m * n - 1);
+    return inits[grid[0][0]].some(c => check(c));
 };
