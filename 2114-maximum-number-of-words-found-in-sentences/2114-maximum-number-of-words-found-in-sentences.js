e8976356003efb6ea37c8dbfc7d5b2ad7f5cafad
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-05-04 10:56:05 +0530

    Time: 0 ms (100%), Space: 55.5 MB (84.27%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+  let maxWords = 0;
+
+  for (let i = 0; i < sentences.length; i++) {
+    let words = sentences[i].split(" ");
+    let wordCount = words.length;
+    
+    if (wordCount > maxWords) {
+      maxWords = wordCount;
+    }
+  }
+
+  return maxWords;
+};
