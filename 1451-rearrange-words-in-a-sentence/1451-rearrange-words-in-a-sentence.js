75db41fe599c2712d94877785c9db1fc2c2d6343
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-05-05 10:54:12 +0530

    Time: 333 ms (8.7%), Space: 59.4 MB (26.09%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.js
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.js
@@ -4,9 +4,8 @@
  */
 var arrangeWords = function(text) {
     let arr=text.toLowerCase().split(" ");
-    let n=arr.length;
-    for(let i=0;i<n-1;i++){
-        for(let j=0;j<n-i-1;j++){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-i-1;j++){
             if(arr[j].length>arr[j+1].length){
 let temp=arr[j];
 arr[j]=arr[j+1];
