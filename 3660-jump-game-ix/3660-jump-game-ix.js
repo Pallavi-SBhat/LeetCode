6adb6f80517b4e3ba36fe50a181d8c8e1cb513f7
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-05-07 18:17:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.js
+++ 3660-jump-game-ix/3660-jump-game-ix.js
@@ -3,38 +3,26 @@
  * @return {number[]}
  */
 var maxValue = function(nums) {
-    const N = nums.length;
-    const stack = [];
-    const ans = Array(N).fill(0);
-    
-    for (let i = 0; i < N; i++) {
-        const currNum = nums[i];
-        
-        // Initialize the current element as its own component
-        let curr = {
-            min: currNum,
-            max: currNum,
-            start: i,
-            end: i
-        };
-        
-        // If the current component can be reached by the previous component, merge them
-        while (stack.length > 0 && curr.min < stack.at(-1).max ) {
-            const popped = stack.pop();
-            curr.min = Math.min(curr.min, popped.min);
-            curr.max = Math.max(curr.max, popped.max);
-            curr.start = popped.start; // Expand the component's boundary backward
-        }
-        
-        stack.push(curr);
+    const n = nums.length;
+    const result = new Array(n);
+    const prefixMax = new Array(n);
+
+    prefixMax[0] = nums[0];
+    for (let i = 1; i < n; i++) {
+        prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
     }
-    
-    // Resolve the maximums for all indices within each component
-    for (let {min, max, start, end} of stack) {
-        for (let i = start; i <= end; i++) {
-            ans[i] = max;
+
+    let suffixMin = Number.MAX_SAFE_INTEGER;
+
+    for (let i = n - 1; i >= 0; i--) {
+        if (prefixMax[i] > suffixMin) {
+            result[i] = (i + 1 < n) ? result[i + 1] : 0;
+        } else {
+            result[i] = prefixMax[i];
         }
+          
+        suffixMin = Math.min(suffixMin, nums[i]);
     }
-    
-    return ans;
+      
+    return result;
 };
