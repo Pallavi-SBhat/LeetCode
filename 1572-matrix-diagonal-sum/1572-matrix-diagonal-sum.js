c32b201df8901cd4777503efe4529e55e4173557
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-05-08 20:24:29 +0530

    Time: 0 ms (100%), Space: 56.8 MB (13.28%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var diagonalSum = function(mat) {
-    let sum=0;
+       let sum=0;
     for(let i=0;i<mat.length;i++){
         for(let j=0;j<mat[i].length;j++){
-            if(i==j||i+j==mat[i].length-1){
-                sum=sum+mat[i][j];
-            }
+           if(i===j|| i===mat.length-1-j){
+            sum+=mat[i][j]
+           }
         }
     }
+
     return sum;
 };
