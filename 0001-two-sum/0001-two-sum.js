00aea473c29d20c12dcf5d898ea98c20a29116f9
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-05-19 10:45:46 +0530

    Time: 73 ms (8.13%), Space: 54.7 MB (60.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,14 +6,10 @@
 var twoSum = function(nums, target) {
   let map = new Map();
 
-    for(let i = 0; i < nums.length; i++){
-        let complement = target - nums[i];
-
-        if(map.has(complement)){
-            return [map.get(complement), i];
-        }
-
-        map.set(nums[i], i);
+  for(let i=0;i<nums.length;i++){
+    for (j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]==target)
+         return [i,j];
     }
-    return [];''
+  }
 };
