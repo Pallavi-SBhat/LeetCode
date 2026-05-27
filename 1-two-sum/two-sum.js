db7a6e2f4c6df523de7793e73f26d4d5d6ba0259
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-05-27 18:46:57 +0530

    Time: 1 ms (85.83%) | Memory: 55.3 MB (40.56%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,11 +5,12 @@
  */
 var twoSum = function(nums, target) {
   let map = new Map();
-
-  for(let i=0;i<nums.length;i++){
-    for (j=i+1;j<nums.length;j++){
-        if(nums[i]+nums[j]==target)
-         return [i,j];
+ for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
     }
-  }
 };
