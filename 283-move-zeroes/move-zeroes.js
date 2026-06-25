ecdd9c0012bc2db44c6331c2f3224ec29dc646dd
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-06-25 08:16:39 +0530

    Time: 2 ms (52.90%) | Memory: 60.5 MB (45.31%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,15 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let left = 0;
-
-    for (let right = 0; right < nums.length; right++) {
-        if (nums[right] !== 0) {
-            nums[left]=nums[right];
-            left++;
+    let i=0,j=0;
+    while(i<nums.length){
+        if(nums[i]!=0){
+            let temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
         }
+        i++;
     }
-  for (let right = left; right < nums.length; right++) {
-        nums[right] = 0;
-    }
+    return nums;
 };
