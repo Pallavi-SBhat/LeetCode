34bf066c84a805b6bbd9c4473ba2438f9d1b29b7
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-06-30 09:44:37 +0530

    Time: 0 ms (100%), Space: 54.4 MB (99.48%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count=0,ans;
+    for(let i=0;i<nums.length;i++){
+        if(count===0) ans=nums[i];
+        if(ans===nums[i]) count++;
+        else count--;
+    }
+    return ans;
+};
